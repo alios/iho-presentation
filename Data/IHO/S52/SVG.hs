ab02b789b01c9aa3e803a578e9b35e666bf069f3
Markus Barenhoff <mbarenh@alios.org>
2014-11-25 17:33:51 +0100

    added height, width viewbox to symbols (+margin)

--- Data/IHO/S52/SVG.hs
+++ Data/IHO/S52/SVG.hs
@@ -11,17 +11,12 @@
 module Data.IHO.S52.SVG where
 
 import Data.IHO.S52.Types
---import Data.IHO.S52.Types
-
 import Text.Blaze.Svg
 import qualified Text.Blaze.Svg11 as SVG
-import Text.Blaze.Svg11 ((!))
 import Text.Blaze.Svg11.Attributes as A
 import Data.Int
 import Data.Text (Text)
 import qualified Data.Text as T
-import Data.Data (Data)
-import Data.Typeable (Typeable)
 import Control.Monad.RWS
 import Control.Lens
 import qualified Data.Map as Map
@@ -57,6 +52,8 @@ mkRenderState _r =
               , _polygonBuffer2 = (Nothing, mempty)
               }
 
+
+
 readVIs :: VectorRecord r => Record r -> Svg
 readVIs _r =
   let s0 = mkRenderState _r
@@ -84,6 +81,7 @@ renderSinglePolygonBuffer (Just p0, pBuffer) _fill  =
   in do
     st <- get
     let penColour = st ^. pen_colour
+        afillOpacity = A.fillOpacity $ SVG.toValue $ toFill $ st ^. fill_trans
         stClass =
           if (_fill) then T.concat ["fill_", penColour]
                      else T.concat ["stroke_", penColour]
@@ -93,25 +91,29 @@ renderSinglePolygonBuffer (Just p0, pBuffer) _fill  =
                      else A.fill $ preEscapedStringValue "none"
     tell $ SVG.path ! aPath ! aClass ! aArg
 
+
+toFill :: (Integral i) => i -> Float
+toFill i =  1 - (fromIntegral i * 0.25)
+
 renderPolygonBuffer :: Bool -> RenderAction ()
 renderPolygonBuffer _fill = do
-  s <- get
+  _s <- get
   tryPolygonBuffer _fill polygonBuffer1
   tryPolygonBuffer _fill polygonBuffer2
   modify (set pen_position $
           maybe (error "unable to restore PenPosition") id $
-          fst $ s ^. polygonBuffer1)
+          fst $ _s ^. polygonBuffer1)
 
 type PolygonBufferLens = Functor f => ((Maybe Vector2, [VectorInstruction]) -> f (Maybe Vector2, [VectorInstruction])) -> RenderState r0 -> f (RenderState r0)
 
 tryPolygonBuffer :: Bool -> PolygonBufferLens -> RenderAction ()
-tryPolygonBuffer _fill l = do
+tryPolygonBuffer _fill _l = do
   _s <- get
-  case (_s ^. l) of
+  case (_s ^. _l) of
    (Nothing, _) -> return ()
    buf@(Just _, _) ->
      do renderSinglePolygonBuffer buf _fill
-        modify (set l (Nothing, []))
+        modify (set _l (Nothing, []))
                          
       
 
@@ -133,14 +135,10 @@ renderVerticeBuffer = do
 readVI :: RenderAction ()
 readVI = ask >>= evalVI
 
-renderPathCmd (PenUp (x, y)) = m x y
-renderPathCmd (PenDraw (x, y)) = l x y
-renderPathCmd c = fail $ "undefined Path Command: " ++ show c
-
-
-
-
-
+renderPathCmd :: VectorInstruction -> Path
+renderPathCmd (PenUp (_x, _y)) = m _x _y
+renderPathCmd (PenDraw (_x, _y)) = l _x _y
+renderPathCmd _c = fail $ "undefined Path Command: " ++ show _c
 
 
 evalVI :: VectorInstruction -> RenderAction ()
@@ -187,7 +185,10 @@ evalVI (PolygonMode PolygonDone) = do
                }
 evalVI (OutlinePolygon) = renderPolygonBuffer False
 evalVI (FillPolygon) = renderPolygonBuffer True
-evalVI (SymbolCall sy o) = return ()
+evalVI (SymbolCall sy o) = do
+  (x,y) <- fmap _pen_position get
+  tell $ useSymbol x y sy -- TODO: implement orientation handling! 
+  
 
 
 
@@ -224,11 +225,11 @@ xlinkns = customAttribute "xmlns:xlink" $
 
 
 
-renderVectorRecordDef :: VectorRecord r => (SVG.Svg -> SVG.Svg) -> Record r -> SVG.Svg
-renderVectorRecordDef ct rec =
+renderVectorRecordDef :: VectorRecord r => Text -> (SVG.Svg -> SVG.Svg) -> Record r -> SVG.Svg
+renderVectorRecordDef pref ct rec =
   let (_px, _py) = vector_pos rec
       vname = vector_name rec
-      idA = A.id_ $ textValue vname
+      idA = A.id_ $ textValue $ mconcat [pref, vname]
       translateS = "translate("++ show (-1 * _px) ++ "," ++ show (-1 * _py) ++ ")"
       translateA = A.transform $ stringValue translateS
   in ct ! idA ! translateA $ do
@@ -241,13 +242,37 @@ renderDefs cschema lib = SVG.defs $ do
   svgColourLib lib cschema
   renderSymbolDefs lib
   renderLineStyleDefs lib
-
+  renderPatternDefs lib
+  
 renderSymbolDefs lib = mconcat $ map renderSymbolDef $ lib_symb lib
 renderLineStyleDefs lib = mconcat $ map renderLineStyleDef $ lib_lnst lib
 renderPatternDefs lib = mconcat $ map renderPatternDef $ lib_patt lib
-renderSymbolDef = renderVectorRecordDef SVG.symbol
-renderLineStyleDef = renderVectorRecordDef SVG.pattern
-renderPatternDef = renderVectorRecordDef SVG.pattern
+renderSymbolDef m =
+  let svg = renderVectorRecordDef "symb_" SVG.symbol m
+      (_x_, _y_) = vector_box_pos m
+      (_w_, _h_) = vector_box_size m
+      lwOff = 30 * 10
+      _x = _x_ - lwOff
+      _y = _y_ - lwOff
+      _w = _w_ + (2 * lwOff)
+      _h = _h_ + (2 * lwOff)
+      vbs = mconcat [ show _x, " ", show _y, " ", show _w, " ", show _h ]
+  in svg ! (A.viewbox $ SVG.toValue vbs) !
+       (A.width $ SVG.toValue . toInteger $ _w) !
+       (A.height $ SVG.toValue . toInteger $ _h) 
+
+
+renderLineStyleDef = renderPatternDef
+renderPatternDef m =
+  let svg = (renderVectorRecordDef "patt_" SVG.pattern m)
+      (_x, _y) = vector_box_pos m
+      (_w, _h) = vector_box_size m
+  in  svg ! (A.patternunits $ textValue "userSpaceOnUse") !
+       (A.x $ SVG.toValue . toInteger $ _x) !
+       (A.y $ SVG.toValue . toInteger $ _x) !
+       (A.width $ SVG.toValue . toInteger $ _w) !
+       (A.height $ SVG.toValue . toInteger $ _h) 
+        
 
 renderVectorRecordDebug :: VectorRecord m => Record m -> Svg
 renderVectorRecordDebug rec = 
@@ -271,180 +296,9 @@ renderVectorRecordDebug rec =
        
 useSymbol :: Integral t => t -> t -> Text -> Svg
 useSymbol _x _y i = 
-  let ref = mconcat [ "#", i ]
+  let ref = mconcat [ "#symb_", i ]
       refA = A.xlinkHref . SVG.toValue $ ref
       xA = A.x . SVG.toValue . toInteger $ _x
       yA = A.y . SVG.toValue . toInteger $ _y      
   in SVG.use ! refA ! xA ! yA
 
-
-
-
-
-
-                           
---  fmap (view pen_colour)
-{-
-
-class VectorInterpreter i where
-    data InterpreterState i :: *
-    defState :: InterpreterState i
-    evalI :: (VectorRecord re) => Record re -> VectorInstruction -> State (InterpreterState i) () 
-    finalizeI :: (VectorRecord re) => Record re -> InterpreterState i -> InterpreterState i
-
-
-
-
-renderIIs :: (VectorRecord re) => Record re -> [[VectorInstruction]] -> Svg
-renderIIs r iis = 
-    let (_, st) = undefined --runState (sequence $ map (map (evalI r) iis) defState)
-        ob = st_outputBuffer $ finalizeI r st
-        outsvg = do
-          _ <- sequence ob
-          return ()          
-    in do
-      outsvg
-
-renderIs :: (VectorRecord re) => Record re -> [VectorInstruction] -> Svg
-renderIs r is = 
-    let (_, st) = runState (sequence $ map (evalI r) is) defState
-        ob = st_outputBuffer $ finalizeI r st
-        outsvg = do
-          _ <- sequence ob
-          return ()          
-    in do
-      outsvg
-
--- (fromString "opacity:0;")
-
-svgInterpreter :: VectorRecord re => Record re -> Svg
-svgInterpreter r = 
-    let iis = vector_vct r
-        cl = class_ $ fromString . T.unpack $ vector_name r
-    in g ! cl $ do
-      _ <- renderIs r $ concat iis
---      _ <- sequence $ map (renderIs r) iis
-      return ()
-
-
-data SVG
-
-data InterpreterMode = None | LineMode
-
-instance VectorInterpreter SVG where
-    data InterpreterState SVG
-        = IS { st_penColour :: Char
-             , st_penWidth :: Int16
-             , st_penTransparency :: Int8
-             , st_penPos :: Vector2
-             , st_penDown :: Bool
-             , st_mode :: InterpreterMode
-             , st_lineBuffer :: [Vector2]
-             , st_outputBuffer :: [Svg]
-             } 
-    defState = IS { st_penColour = undefined
-                  , st_penWidth = 1
-                  , st_penTransparency = 0
-                  , st_penPos = (0,0)
-                  , st_penDown = False
-                  , st_mode = None
-                  , st_lineBuffer = []
-                  , st_outputBuffer = []
-                  }
-    finalizeI re st =
-        case (st_mode st) of
-          None -> st
-          LineMode -> mkPolyLine re st
-
-    evalI _ (SetPenColour col) = modify (\st' -> st' { st_penColour = col})
-    evalI _ (SetPenTransparency pt) = modify (\st' -> st' { st_penTransparency = pt})
-    evalI _ (SetPenWidth w) = modify (\st' -> st' { st_penWidth = w } )
-    evalI re (PenUp p) = do
-      st <- get
-      case (st_mode st) of
-        None -> return ()
-        LineMode -> modify $ mkPolyLine re
-      modify (\st' -> st' { st_penPos = p, st_penDown = False })
-    evalI _ (PenDraw p) = do
-      st <- get
-      case (st_mode st) of
-        None -> modify(\st' -> st' { st_penPos = p
-                                 , st_lineBuffer = [st_penPos st, p]
-                                 , st_mode = LineMode })
-        LineMode -> modify(\st' -> st' { st_penPos = p
-                                     , st_lineBuffer = st_lineBuffer st ++ [p] })
-    -- TODO not implemented yet                   
-    evalI re (Circle rad) = modify $ mkCircle re rad
-    evalI _ (PolygonMode pm) = return ()
-    evalI _ (OutlinePolygon) = return ()
-    evalI _ (FillPolygon) = return ()
-    evalI _ (SymbolCall symb ori) = return ()
-
-
-floatAttrMM2 :: Float -> AttributeValue
-floatAttrMM2 = fromString . printf "%0.2fmm"
-floatAttrMM1 :: Float -> AttributeValue
-floatAttrMM1 = fromString . printf "%0.1fmm"
-
-
-mkCircle :: VectorRecord m =>
-  Record m -> Int16 -> InterpreterState SVG -> InterpreterState SVG
-mkCircle re rad st' = 
-    let rada = A.r $ fromString $ show rad
-        (x', y') = st_penPos st'
-        xa = A.cx $ fromString $ show x'
-        ya = A.cy $ fromString $ show y'
-        col = case (Map.lookup (st_penColour st') (vector_color_refs re)) of
-              Nothing -> error $ printf "unknown penColour: %c" (st_penColour st')
-              Just col' -> col'
-        cl = (A.class_ $ fromString $ T.unpack col) 
-
-        pw = strokeWidth $ fromString $ show $ 30 * st_penWidth st'
-
-        csvg = circle ! pw ! cl ! xa ! ya ! rada 
-    in st' { st_outputBuffer = csvg : st_outputBuffer st' }
-
-
-mkPolyLine :: VectorRecord m =>
-  Record m -> InterpreterState SVG -> InterpreterState SVG
-mkPolyLine re st = 
-    let ln = polyline ! pw ! ps ! cl
-        ps = (A.points $ showV2s . st_lineBuffer $ st)
-        pw = strokeWidth $ fromString $ show $ 30 * st_penWidth st
-        col = case (Map.lookup (st_penColour st) (vector_color_refs re)) of
-              Nothing -> error $ printf "unknown penColour: %c" (st_penColour st)
-              Just col' -> col'
-        cl = (A.class_ $ fromString $ T.unpack col) 
-        ob = st_outputBuffer st ++ if ((length .st_lineBuffer $ st) >= 2)
-                                   then [ln] 
-                                   else error "PenUp with only 1 entry in linebuffer"
-    in st { st_outputBuffer =  ob, st_lineBuffer = [], st_mode = None }
-
-showV2s :: [Vector2] -> AttributeValue
-showV2s = fromString . showV2s'
-
-showV2s' :: [Vector2] -> String
-showV2s' = concat . intersperse " "  . map showV2
-
-showV2 :: Vector2 -> String
-showV2 (vx,vy) = concat [ show vx, ",", show vy]
-
-
-
-renderR :: VectorRecord m =>
-          Record m -> ByteString
-renderR re = renderMarkup $ svgInterpreter re
-
-
-t1_is = renderR  trec2
-
-
-
-trec2 = SymbolEntry {symb_modn = "SY", symb_rcid = 1425, symb_stat = "NIL", symb_synm = "ACHBRT07", symb_sydf = VectorDrawing, symb_sycl = 1264, symb_syrw = 1062, symb_syhl = 506, symb_syvl = 506, symb_sbxc = 1010, symb_sbxr = 783, symb_sxpo = "designated anchor berth for a single vessel", symb_scrf = [('A',"CHMGD")], symb_sbtm = [], symb_svct = [[SetPenColour 'A',SetPenWidth 1,PenUp (1262,783),PenDraw (1262,1004)],[SetPenColour 'A',SetPenWidth 1,PenUp (1262,1289),PenDraw (1262,1207)],[SetPenColour 'A',SetPenWidth 1,PenUp (1516,1135),PenDraw (1364,1287),PenDraw (1167,1287),PenDraw (1010,1132)],[SetPenColour 'A',SetPenWidth 1,PenUp (1261,1101),Circle 101],[SetPenColour 'A',SetPenWidth 1,PenUp (1107,937),PenDraw (1424,937)]]}
-
-trec :: Record Symbol
-trec = SymbolEntry {symb_modn = "SY", symb_rcid = 1426, symb_stat = "NIL", symb_synm = "ACHRES51", symb_sydf = VectorDrawing, symb_sycl = 981, symb_syrw = 958, symb_syhl = 1229, symb_syvl = 1304, symb_sbxc = 1621, symb_sbxr = 1471, symb_sxpo = "area where anchoring is prohibited or restricted", symb_scrf = [('A',"CHMGF")], symb_sbtm = [], symb_svct = [[SetPenColour 'A',SetPenWidth 1,PenUp (2193,1471),PenDraw (2193,1800),PenDraw (1875,1800),PenDraw (1875,1912),PenDraw (2193,1912),PenDraw (2193,2596),PenDraw (1987,2550),PenDraw (1800,2409),PenDraw (1621,2409),PenDraw (1875,2596),PenDraw (2250,2775),PenDraw (2596,2596),PenDraw (2850,2409),PenDraw (2700,2409),PenDraw (2475,2550),PenDraw (2268,2596),PenDraw (2268,1912),PenDraw (2596,1912),PenDraw (2596,1800),PenDraw (2268,1800),PenDraw (2268,1471),PenDraw (2193,1471)],[SetPenColour 'A',SetPenWidth 3,PenUp (2703,1671),PenDraw (1701,2679)]]}
-
-
-
--}
